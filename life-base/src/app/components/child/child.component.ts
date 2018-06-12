86670ed2ca8f5f1c5d08f951641e132d1fd6af12
Nemanja Bacic <nbacic@ymail.com>
2018-06-12 08:56:26 +0200

    Added Stripe payment system.

--- life-base/src/app/components/child/child.component.ts
+++ life-base/src/app/components/child/child.component.ts
@@ -61,6 +61,36 @@ $(document).ready(function(){
           });
       });
   }
+      var handler = StripeCheckout.configure({
+          key: 'pk_test_QvHMW8tDmDlsoHiKsyt4P77i',
+          image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+          locale: 'auto',
+          token: function(token) {
+              // You can access the token ID with `token.id`.
+              // Get the token ID to your server-side code for use.
+              // console.log(token);
+              localStorage.setItem('token', JSON.stringify(token.id));
+              console.log(localStorage.getItem('token'));
+          }
+      });
+
+      document.getElementById('customButton').addEventListener('click', function(e) {
+          // Open Checkout with further options:
+          handler.open({
+              name: 'Life Base',
+              description: 'Description text',
+              amount: 18000,
+              allowRememberMe: false
+          });
+          e.preventDefault();
+      });
+
+      // Close Checkout on page navigation:
+      if(localStorage.getItem('token')) {
+          window.addEventListener('popstate', function() {
+              handler.close();
+          });
+      }
 
  
   }
